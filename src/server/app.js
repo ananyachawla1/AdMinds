27275ce156d9bf495ba77ce3d38378b760062449
ananyachawla1 <108053283+ananyachawla1@users.noreply.github.com>
2024-02-03 22:32:09 +0530

    call pixel api

--- src/server/app.js
+++ src/server/app.js
@@ -1,11 +1,90 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
+const axios = require("axios");
+const bluebird = require('bluebird')
+app.use(express.json()); //parsing response from lg api
+const headers = {
+  "Content-Type": "application/json",
+  Authorization: "Bearer Bj8YrhQMj", //validation of user
+};
+app.get("/example", (req, res) => {
+  res.send("Hello World!");
+});
 
-app.get('/example', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/pixel/sample", (req, res) => {
+  axios
+    .get("https://cla-pixel.lgads.tv/sample", { headers })
+    .then((data) => {
+      console.log("data", data);
+      res.status(200).send(data.data);
+      // res.json(data)
+    })
+    .catch((err) => res.send(err));
+});
 
+app.get("/pixel/list/date", (req, res) => {
+  axios
+    .get("https://cla-pixel.lgads.tv/list/date", { headers })
+    .then((data) => {
+      console.log("data", data);
+      res.status(200).send(data.data);
+      // res.json(data)
+    })
+    .catch((err) => res.send(err));
+});
+app.get("/pixel/list/deviceId", (req, res) => {
+  axios
+    .get("https://cla-pixel.lgads.tv/list/deviceId", { headers })
+    .then((data) => {
+      console.log("data", data);
+      res.status(200).send(data.data);
+      // res.json(data)
+    })
+    .catch((err) => res.send(err));
+});
+
+
+app.get("/pixel/list/stationId", (req, res) => {
+  axios
+    .get("https://cla-pixel.lgads.tv/list/stationId", { headers })
+    .then((data) => {
+      console.log("data", data);
+      res.status(200).send(data.data);
+      // res.json(data)
+    })
+    .catch((err) => res.send(err));
+});
+app.get('/epg/recommendation', (req, res) => {
+    const deviceId = req.query.deviceId;
+const data = req.query.date;
+    axios.get(`https://cla-pixel.lgads.tv/pixels?deviceId=${deviceId}&date=2024-01-01`, { headers })
+      .then(async data => {
+        // console.log("Pixel data....", data);
+        // res.status(200).json(data.data);
+        // console.log("Pixel data....", data);
+      // hit EPG Listing looping over the array received in above pixel request, pick each stationId. Collect all promises.
+      const promises = data.data.map((pixelItem, index) => {//promises array of all api calls
+        // console.log("Pixel itemm....", pixelItem);
+        return axios.get(`https://cla-epg.lgads.tv/epg/listings?stationId=${pixelItem.stationId}`, { headers }).then((data) => {
+          return data.data;
+        });
+      })
+      console.log("Pixel promisees...", promises);
+      const recommendedEpgList = await Promise.all(promises).then(values => { //waiting for promises to resolve/reject
+        console.log("EPG Listing Values.....", values);
+        return values;//all promises resolved
+      }).catch(err => {
+        console.log(err);
+        res.send("Error while fetching EPG listing")
+      })
+      res.status(200).json(recommendedEpgList)
+      }).catch(err => {
+        console.log(err);
+        res.status(400).send("error while fetching pixel data");
+      })
+  })
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
+//&date=2024-01-01
