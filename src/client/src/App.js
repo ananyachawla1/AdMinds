56f087a9c8661eaef495b3ad49355705e7b9ec5f
Abhijanitha <raviralaabhijanitha.com>
2024-02-03 17:52:05 +0530

    6th commit

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -24,27 +24,132 @@
 
 // export default App;
 
-// src/App.js
-// src/App.js
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Profile from './components/Profile';
-import Movies from './components/Movies';
 
-const user = { name: 'John' }; // Placeholder user data
+// // src/App.js
+// import React from 'react';
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import Navbar from './components/Navbar';
+// import Profile from './components/Profile';
+// import Movies from './components/Movies';
+
+// const user = { name: 'John' }; // Placeholder user data
+
+// const App = () => {
+//   return (
+//     <Router>
+//       <Navbar />
+//       <Routes>
+//         <Route path="/profile" element={<Profile user={user} />} />
+//         <Route path="/movies" element={<Movies />} />
+//         <Route path="/" element={<Home />} />
+//       </Routes>
+//     </Router>
+//   );
+// };
+
+// const Home = () => (
+//   <div>
+//     <h2>Home</h2>
+//     {/* Add any other content for the Home page */}
+//     <Movies />
+//   </div>
+// );
+
+// export default App;
+
+import React, { useState, useEffect } from 'react';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import MovieList from './components/MovieList';
+import MovieListHeading from './components/MovieListHeading';
+import SearchBox from './components/SearchBox';
+import AddFavourites from './components/AddFavourites';
+import RemoveFavourites from './components/RemoveFavourites';
+// import NavBar from './components/NavBar';
 
 const App = () => {
-  return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/profile" element={<Profile user={user} />} />
-        <Route path="/movies" element={<Movies />} />
-        <Route path="/" element={<h2>Home</h2>} />
-      </Routes>
-    </Router>
-  );
+	const [movies, setMovies] = useState([]);
+	const [favourites, setFavourites] = useState([]);
+	const [searchValue, setSearchValue] = useState('');
+
+	const getMovieRequest = async (searchValue) => {
+		const url = "https://cla-epg.lgads.tv/epg/listings?genre=Action&showType=movie" 
+
+		const response = await fetch(url,{
+			headers: {
+			  'Content-Type': 'application/json',
+			  'Authorization':'Bearer QZYzxZ1sc'
+			}
+		  });
+		const responseJson = await response.json();
+
+		if (responseJson.result) {
+			setMovies(responseJson.result);
+		}
+	};
+
+	useEffect(() => {
+		getMovieRequest(searchValue);
+	}, [searchValue]);
+
+	useEffect(() => {
+		const movieFavourites = JSON.parse(
+			localStorage.getItem('react-movie-app-favourites')
+		);
+
+		if (movieFavourites) {
+			setFavourites(movieFavourites);
+		}
+	}, []);
+	console.log(movies);
+
+	const saveToLocalStorage = (items) => {
+		localStorage.setItem('react-movie-app-favourites', JSON.stringify(items));
+	};
+
+	const addFavouriteMovie = (movie) => {
+		const newFavouriteList = [...favourites, movie];
+		setFavourites(newFavouriteList);
+		saveToLocalStorage(newFavouriteList);
+	};
+
+	const removeFavouriteMovie = (movie) => {
+		const newFavouriteList = favourites.filter(
+			(favourite) => favourite.imdbID !== movie.imdbID
+		);
+
+		setFavourites(newFavouriteList);
+		saveToLocalStorage(newFavouriteList);
+	};
+
+	return (
+		<div className='container-fluid movie-app'>
+      {/* <NavBar/> */}
+      {/* <Route path="/home" exact component={Home} /> */}
+			<div className='row d-flex align-items-center mt-4 mb-4'>
+				<MovieListHeading heading='Movies' />
+				<SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
+			</div>
+			<div className='row'>
+				<MovieList
+					movies={movies}
+					handleFavouritesClick={addFavouriteMovie}
+					favouriteComponent={AddFavourites}
+				/>
+			</div>
+			<div className='row d-flex align-items-center mt-4 mb-4'>
+				<MovieListHeading heading='Favourites' />
+			</div>
+			<div className='row'>
+				<MovieList
+					movies={favourites}
+					handleFavouritesClick={removeFavouriteMovie}
+					favouriteComponent={RemoveFavourites}
+				/>
+			</div>
+		</div>
+	);
 };
 
 export default App;
+
