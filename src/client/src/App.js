d2c4239157f9123f824670d62425ad7fa437de5a
Abhijanitha <raviralaabhijanitha.com>
2024-02-03 22:30:15 +0530

    7th commit

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -1,155 +1,108 @@
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-
-// // src/App.js
-// import React from 'react';
-// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-// import Navbar from './components/Navbar';
-// import Profile from './components/Profile';
-// import Movies from './components/Movies';
-
-// const user = { name: 'John' }; // Placeholder user data
-
-// const App = () => {
-//   return (
-//     <Router>
-//       <Navbar />
-//       <Routes>
-//         <Route path="/profile" element={<Profile user={user} />} />
-//         <Route path="/movies" element={<Movies />} />
-//         <Route path="/" element={<Home />} />
-//       </Routes>
-//     </Router>
-//   );
-// };
-
-// const Home = () => (
-//   <div>
-//     <h2>Home</h2>
-//     {/* Add any other content for the Home page */}
-//     <Movies />
-//   </div>
-// );
-
-// export default App;
-
 import React, { useState, useEffect } from 'react';
-// import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import MovieList from './components/MovieList';
 import MovieListHeading from './components/MovieListHeading';
 import SearchBox from './components/SearchBox';
 import AddFavourites from './components/AddFavourites';
 import RemoveFavourites from './components/RemoveFavourites';
-// import NavBar from './components/NavBar';
 
 const App = () => {
-	const [movies, setMovies] = useState([]);
-	const [favourites, setFavourites] = useState([]);
-	const [searchValue, setSearchValue] = useState('');
+  const [ActionMovies, setActionMovies] = useState([]);
+  const [ThrillerMovies, setThrillerMovies] = useState([]);
+  const [ComedyMovies, setComedyMovies] = useState([]);
+  const [favourites, setFavourites] = useState([]);
+  const [searchValue, setSearchValue] = useState('');
 
-	const getMovieRequest = async (searchValue) => {
-		const url = "https://cla-epg.lgads.tv/epg/listings?genre=Action&showType=movie" 
+  const getMovieRequest = async (genre) => {
+    const url = `https://cla-epg.lgads.tv/epg/listings?genre=${genre}&showType=movie`;
 
-		const response = await fetch(url,{
-			headers: {
-			  'Content-Type': 'application/json',
-			  'Authorization':'Bearer QZYzxZ1sc'
-			}
-		  });
-		const responseJson = await response.json();
+    const response = await fetch(url, {
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer QZYzxZ1sc'
+      }
+    });
 
-		if (responseJson.result) {
-			setMovies(responseJson.result);
-		}
-	};
+    const responseJson = await response.json();
+    return responseJson.result || [];
+  };
 
-	useEffect(() => {
-		getMovieRequest(searchValue);
-	}, [searchValue]);
+  useEffect(() => {
+    getMovieRequest("Action").then((movies) => setActionMovies(movies));
+    getMovieRequest("Thriller").then((movies) => setThrillerMovies(movies));
+    getMovieRequest("Comedy").then((movies) => setComedyMovies(movies));
+  }, []);
 
-	useEffect(() => {
-		const movieFavourites = JSON.parse(
-			localStorage.getItem('react-movie-app-favourites')
-		);
+  useEffect(() => {
+    const movieFavourites = JSON.parse(localStorage.getItem('react-movie-app-favourites')) || [];
+    setFavourites(movieFavourites);
+  }, []);
 
-		if (movieFavourites) {
-			setFavourites(movieFavourites);
-		}
-	}, []);
-	console.log(movies);
+  const saveToLocalStorage = (items) => {
+    localStorage.setItem('react-movie-app-favourites', JSON.stringify(items));
+  };
 
-	const saveToLocalStorage = (items) => {
-		localStorage.setItem('react-movie-app-favourites', JSON.stringify(items));
-	};
+  const addFavouriteMovie = (movie) => {
+    const newFavouriteList = [...favourites, movie];
+    setFavourites(newFavouriteList);
+    saveToLocalStorage(newFavouriteList);
+  };
 
-	const addFavouriteMovie = (movie) => {
-		const newFavouriteList = [...favourites, movie];
-		setFavourites(newFavouriteList);
-		saveToLocalStorage(newFavouriteList);
-	};
+  const removeFavouriteMovie = (movie) => {
+    const newFavouriteList = favourites.filter(
+      (favourite) => favourite.imdbID !== movie.imdbID
+    );
 
-	const removeFavouriteMovie = (movie) => {
-		const newFavouriteList = favourites.filter(
-			(favourite) => favourite.imdbID !== movie.imdbID
-		);
+    setFavourites(newFavouriteList);
+    saveToLocalStorage(newFavouriteList);
+  };
 
-		setFavourites(newFavouriteList);
-		saveToLocalStorage(newFavouriteList);
-	};
+  const filterMoviesByQuery = (movies) => {
+    return movies.filter((movie) =>
+      movie.programInfo.programTitle.toLowerCase().startsWith(searchValue.toLowerCase())
+    );
+  };
 
-	return (
-		<div className='container-fluid movie-app'>
-      {/* <NavBar/> */}
-      {/* <Route path="/home" exact component={Home} /> */}
-			<div className='row d-flex align-items-center mt-4 mb-4'>
-				<MovieListHeading heading='Movies' />
-				<SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
-			</div>
-			<div className='row'>
-				<MovieList
-					movies={movies}
-					handleFavouritesClick={addFavouriteMovie}
-					favouriteComponent={AddFavourites}
-				/>
-			</div>
-			<div className='row d-flex align-items-center mt-4 mb-4'>
-				<MovieListHeading heading='Favourites' />
-			</div>
-			<div className='row'>
-				<MovieList
-					movies={favourites}
-					handleFavouritesClick={removeFavouriteMovie}
-					favouriteComponent={RemoveFavourites}
-				/>
-			</div>
-		</div>
-	);
+  return (
+    <div className='container-fluid movie-app'>
+      <div className='row d-flex align-items-center mt-4 mb-4'>
+        <MovieListHeading heading='Movies' />
+        <SearchBox value={searchValue} onChange={setSearchValue} />
+      </div>
+      <div className='row'>
+        <h2>Action Movies</h2>
+        <MovieList
+          movies={filterMoviesByQuery(ActionMovies)}
+          handleFavouritesClick={addFavouriteMovie}
+          favouriteComponent={AddFavourites}
+        />
+        <br />
+        <h2>Thriller Movies</h2>
+        <MovieList
+          movies={filterMoviesByQuery(ThrillerMovies)}
+          handleFavouritesClick={addFavouriteMovie}
+          favouriteComponent={AddFavourites}
+        />
+        <br />
+        <h2>Comedy Movies</h2>
+        <MovieList
+          movies={filterMoviesByQuery(ComedyMovies)}
+          handleFavouritesClick={addFavouriteMovie}
+          favouriteComponent={AddFavourites}
+        />
+      </div>
+      <div className='row d-flex align-items-center mt-4 mb-4'>
+        <MovieListHeading heading='Favourites' />
+      </div>
+      <div className='row'>
+        <MovieList
+          movies={favourites}
+          handleFavouritesClick={removeFavouriteMovie}
+          favouriteComponent={RemoveFavourites}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default App;
-
--- src/client/src/components/MovieList.js
+++ src/client/src/components/MovieList.js
@@ -1,23 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const MovieList = (props) => {
-	const FavouriteComponent = props.favouriteComponent;
+  const FavouriteComponent = props.favouriteComponent;
+  const [uniqueTitles, setUniqueTitles] = useState(new Set());
 
-	return (
-		<>
-			{props.movies.map((movie, index) => (
-				<div className='image-container d-flex justify-content-start m-3'>
-					<img src={movie.Poster} alt='movie'></img>
-					<div
-						onClick={() => props.handleFavouritesClick(movie)}
-						className='overlay d-flex align-items-center justify-content-center'
-					>
-						<FavouriteComponent />
-					</div>
-				</div>
-			))}
-		</>
-	);
+  const handleFavouritesClick = (movie) => {
+    props.handleFavouritesClick(movie);
+    // Add the movie title to the set to track unique titles
+    setUniqueTitles((prevTitles) => new Set([...prevTitles, movie.programInfo.programTitle]));
+  };
+
+  return (
+    <div className='d-flex flex-wrap justify-content-start' style={{ display: 'flex', flexDirection: 'row' }}>
+      {props.movies.map((movie, index) => (
+        // Only render the movie if its title is not in the set of unique titles
+        !uniqueTitles.has(movie.programInfo.programTitle) && (
+          <div className='image-container m-3' key={index} style={{ flex: '0 0 15%', width: '100px', padding:20 }}>
+            {movie.programInfo && movie.programInfo.images && movie.programInfo.images.length > 0 && (
+              <img
+                src={movie.programInfo.images[0].url}
+                alt='movie'
+                height='200'
+                width='150'
+                style={{ objectFit: 'cover', width: '100%', height: '100%' }}
+              />
+            )}
+            {(!movie.programInfo || !movie.programInfo.images || movie.programInfo.images.length === 0) && (
+              <img
+                src="https://imgs.search.brave.com/bTQyZH99S8mTAIff7nLrbZktAnBvaH3n0o6M0s0kEr4/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMuZmFuZGFuZ28u/Y29tL2ltYWdlcy9m/YW5kYW5nb2Jsb2cv/TW9vbmZhbGxfUGF5/b2ZmX1ZlcnRpY2Fs/X0tleUFydF8yMDFf/RklOMDhfU0xGX2Jy/aWdodF9GSU5fc20u/anBn" // Placeholder image URL
+                alt='placeholder'
+                height='200'
+                width='150'
+                style={{ objectFit: 'cover', width: '100%', height: '100%' }}
+              />
+            )}
+            <div
+              onClick={() => handleFavouritesClick(movie)}
+              className='overlay d-flex align-items-center justify-content-center'
+            >
+              {FavouriteComponent && <FavouriteComponent />}
+            </div>
+            <div>{movie.programInfo && movie.programInfo.programTitle}</div>
+          </div>
+        )
+      ))}
+    </div>
+  );
 };
 
 export default MovieList;
